836601b0266b5e1befe0855f64d4092271b224b4
Alex <badnwz.ad@gmail.com>
2021-01-24 08:06:39 +0200

    raphael: switch to redfin october 2021 fingerprint
    Signed-off-by: Alex <badnwz.ad@gmail.com>

--- init/init_raphael.cpp
+++ init/init_raphael.cpp
@@ -60,8 +60,7 @@ void load_raphaelglobal() {
     property_override("ro.product.system.model", "Mi 9T Pro");
     property_override("ro.build.product", "raphael");
     property_override("ro.product.device", "raphael");
-    property_override("ro.build.description", "raphael-user 9 PKQ1.181121.001 V10.3.1.0.PFKEUXM release-keys");
-    property_override("ro.build.fingerprint", "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys");
+    property_override("ro.build.description", "redfin-user 11 RQ3A.211001.001 7641976 release-keys");
 }
 
 void load_raphaelin() {
@@ -69,8 +68,7 @@ void load_raphaelin() {
     property_override("ro.product.system.model", "Redmi K20 Pro");
     property_override("ro.build.product", "raphaelin");
     property_override("ro.product.device", "raphaelin");
-    property_override("ro.build.description", "raphaelin-user 9 PKQ1.181121.001 V10.3.3.0.PFKINXM release-keys");
-    property_override("ro.build.fingerprint", "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys");
+    property_override("ro.build.description", "redfin-user 11 RQ3A.211001.001 7641976 release-keys");
 }
 
 void load_raphael() {
@@ -78,8 +76,7 @@ void load_raphael() {
     property_override("ro.product.system.model", "Redmi K20 Pro");
     property_override("ro.build.product", "raphael");
     property_override("ro.product.device", "raphael");
-    property_override("ro.build.description", "raphael-user 9 PKQ1.181121.001 V10.3.12.0.PFKCNXM release-keys");
-    property_override("ro.build.fingerprint", "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys");
+    property_override("ro.build.description", "redfin-user 11 RQ3A.211001.001 7641976 release-keys");
 }
 
 
@@ -95,6 +92,6 @@ void vendor_load_properties() {
     } else {
         LOG(ERROR) << __func__ << ": unexcepted region!";
     }
-
+    property_override("ro.build.fingerprint", "google/redfin/redfin:11/RQ3A.211001.001/7641976:user/release-keys");
     load_dalvik_properties();
 }
