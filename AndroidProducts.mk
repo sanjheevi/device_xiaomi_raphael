c3046812761edad70a5b477e0e18068a80cbcf8d
zoldycc <sanjheevisv@palladiumos.com>
2021-10-30 17:02:55 +0530

    raphael: palladium bring-up

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/wave_raphael.mk
+    $(LOCAL_DIR)/palladium_raphael.mk
+
+COMMON_LUNCH_CHOICES := \
+    palladium_raphael-user \
+    palladium_raphael-userdebug \
+    palladium_raphael-eng
--- palladium_raphael.mk
+++ palladium_raphael.mk
@@ -6,15 +6,16 @@
 
 $(call inherit-product, device/xiaomi/raphael/device.mk)
 
-# Inherit some common Wave stuff.
-$(call inherit-product, vendor/wave/configs/common.mk)
+# Inherit some common Palladium-OS stuff.
+$(call inherit-product, vendor/palladium/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
+PALLADIUM_BUILD_TYPE := OFFICIAL
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := raphael
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := wave_raphael
+PRODUCT_NAME := palladium_raphael
 
 BUILD_FINGERPRINT := "Xiaomi/raphaelin/raphaelin:9/PKQ1.181121.001/V10.3.3.0.PFKINXM:user/release-keys"
 
